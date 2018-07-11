0881b000bfec4f909d431fb966fff754ffe22086
rafysanchez <rafysanchez@hotmail.com>
2018-07-11 18:45:21 -0300

    ajustes e correçoes
    ajustes e correçoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , ViewChild } from '@angular/core';
 import { Popup } from 'ng2-opd-popup';
 
 @Component({
@@ -7,9 +7,49 @@ import { Popup } from 'ng2-opd-popup';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private popup: Popup) { }
+  @ViewChild('popup0') popup0: Popup;
+  @ViewChild('popup1') popup1: Popup;
+  @ViewChild('popup2') popup2: Popup;
+  @ViewChild('popup4') popup4: Popup;
+
+  constructor(private popup: Popup) {
+    this.popup.options = {
+      header: 'Your custom header',
+      color: '#5cb85c', // red, blue....
+      widthProsentage: 40, // The with of the popou measured by browser width
+      animationDuration: 1, // in seconds, 0 = no animation
+      showButtons: true, // You can hide this in case you want to use custom buttons
+      confirmBtnContent: 'OK', // The text on your confirm button
+      cancleBtnContent: 'Cancel', // the text on your cancel button
+      confirmBtnClass: 'btn btn-default', // your class for styling the confirm button
+      cancleBtnClass: 'btn btn-default', // you class for styling the cancel button
+      animation: 'fadeInDown' // 'fadeInLeft', 'fadeInRight', 'fadeInUp', 'bounceIn','bounceInDown'
+  };
+
+   // this.popup.show(this.popup.options);
+   }
 
   showPopup() {
-    this.popup.show();
+    this.popup0.show();
+  }
+
+  ClickButton() {
+    this.popup1.show();
+  }
+
+  ClickAnotherButton() {
+    this.popup2.show();
+  }
+
+  showPopup2() {
+    this.popup4.show(this.popup.options);
+  }
+
+  YourConfirmEvent() {
+    alert('You cliked confirm');
+  }
+
+  YourCancelEvent() {
+    alert('You cliked cancel');
   }
 }
